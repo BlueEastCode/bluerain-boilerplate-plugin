39a71ff7f990d3a1b09a6fc6d14381d1c0246d2f
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-03-20 12:26:12 +0500

    build: Fixed build issues

--- test/stories/DummyComponent.stories.tsx
+++ test/stories/DummyComponent.stories.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { storiesOf } from '@blueeast/bluerain-storybook-addon';
-
 import DummyComponent from '../../src/components/DummyComponent';
+import React from 'react';
+import storiesOf from '@blueeast/bluerain-storybook-addon';
 
 storiesOf('DummyComponent', module)
   .add('Simple', () => <DummyComponent />);

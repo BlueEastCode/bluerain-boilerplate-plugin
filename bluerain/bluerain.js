bd92cb7730b06c757ef498bab8115f0287fcf020
nasir-abbas <nasir.abbas@blueeast.com>
2018-06-29 11:46:38 +0500

    add welcome message to boilerplate

--- bluerain/bluerain.js
+++ bluerain/bluerain.js
@@ -1,1 +1,33 @@
-console.log("Welcome to Bluerain");
+// This file contain all the apps, plugins and configuration which are required
+// for booting bluerain-os. see https://blueeast.gitbooks.io/bluerain-os/
+// module.exports = {
+// 	platform: [
+// 		require('@blueeast/bluerain-platform-react-native-web')
+// 	],
+// 	apps: [
+// 		// All bluerain apps will also be added in this array
+// 	],
+// 	plugins: [
+// 		// All bluerain plugins will be added here
+// 		require('@blueeast/bluerain-plugin-react-router'),
+// 		require('@blueeast/bluerain-plugin-redux'),
+// 		require('@blueeast/bluerain-plugin-redux-devtools'),
+// 		require('@blueeast/bluerain-plugin-responsive-components'),
+// 		require('./dist'),
+// 	],
+// 	config: {
+// 		// Configurations for bluerain-os will be added here
+// 		title: 'BlueRain Plugin Boilerplate',
+// 	}
+// };
+
+// function greeter(person) {
+//     return "Hello, " + person;
+// }
+
+const greetings = user => console.log(`Hello ${user}!`);
+
+let user = "Blueeast";
+console.log('user', typeof greetings);
+
+document.body.innerHTML = greetings(user);

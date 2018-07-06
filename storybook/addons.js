d31539530e9347d92edca084a6f7dc5e061480d2
adnan1naeem <adnan.naeem@blueeast.com>
2018-07-06 17:18:52 +0500

    fix(added addons in storybook): added addons in storybook
    added addons in storybook

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,3 +1,20 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register'
+import '@storybook/addon-storyshots/register';
+import '@storybook/addon-console';
+import '@storybook/addon-viewport/register'
+import 'storybook-readme/register';
+import 'react-storybook-addon-props-combinations/register';
+import 'storybook-addon-intl/register';
+import 'storybook-addon-specifications/register';
+import { configure } from '@storybook/react';
+import 'storybook-chrome-screenshot/register';
+import 'storybook-addon-styled-component-theme/dist/register';
+
+setDefaults({
+  // overwrite global defaults here
+})
+
+configure(() => {
+  // ...
+}, module)
